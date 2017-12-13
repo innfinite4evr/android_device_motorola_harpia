7fd659d997ea91be6e64c684cafc436dc8d834a2
innfinite4evr <innfinite4vr@gmail.com>
2017-12-13 09:23:51 +0000

    harpia: Update init for 8.1
    Signed-off-by: innfinite4evr <innfinite4vr@gmail.com>

--- init/init_harpia.cpp
+++ init/init_harpia.cpp
@@ -29,7 +29,6 @@
  */
 
 #include <stdlib.h>
-#include <stdio.h>
 #include <android-base/properties.h>
 #include "vendor_init.h"
 #include "property_service.h"
@@ -75,35 +74,35 @@ void vendor_load_properties()
 
     dualsim = GetProperty("ro.boot.dualsim","");
     if (dualsim == "true") {
-        android::init::property_set("persist.radio.force_get_pref", "1");
-        android::init::property_set("persist.radio.multisim.config", "dsds");
-        android::init::property_set("ro.hw.dualsim", "true");
+        property_set("persist.radio.force_get_pref", "1");
+        property_set("persist.radio.multisim.config", "dsds");
+        property_set("ro.hw.dualsim", "true");
         msim = true;
     }
 
     bootdevice = GetProperty("ro.boot.device","");
-    android::init:property_set("ro.hw.device", bootdevice.c_str());
+    property_set("ro.hw.device", bootdevice.c_str());
 
     radio = GetProperty("ro.boot.radio","");
-    android::init::property_set("ro.hw.radio", radio.c_str());
+    property_set("ro.hw.radio", radio.c_str());
 
     if (is2GB()) {
-        android::init::property_set("dalvik.vm.heapstartsize", "8m");
-        android::init::property_set("dalvik.vm.heapgrowthlimit", "192m");
-        android::init::property_set("dalvik.vm.heapsize", "512m");
-        android::init::property_set("dalvik.vm.heaptargetutilization", "0.75");
-        android::init::property_set("dalvik.vm.heapminfree", "512k");
-        android::init::property_set("dalvik.vm.heapmaxfree", "8m");
+        property_set("dalvik.vm.heapstartsize", "8m");
+        property_set("dalvik.vm.heapgrowthlimit", "192m");
+        property_set("dalvik.vm.heapsize", "512m");
+        property_set("dalvik.vm.heaptargetutilization", "0.75");
+        property_set("dalvik.vm.heapminfree", "512k");
+        property_set("dalvik.vm.heapmaxfree", "8m");
     } else {
-        android::init::property_set("dalvik.vm.heapstartsize", "8m");
-        android::init::property_set("dalvik.vm.heapgrowthlimit", "96m");
-        android::init::property_set("dalvik.vm.heapsize", "256m");
-        android::init::property_set("dalvik.vm.heaptargetutilization", "0.75");
-        android::init::property_set("dalvik.vm.heapminfree", "2m");
-        android::init::property_set("dalvik.vm.heapmaxfree", "8m");
+        property_set("dalvik.vm.heapstartsize", "8m");
+        property_set("dalvik.vm.heapgrowthlimit", "96m");
+        property_set("dalvik.vm.heapsize", "256m");
+        property_set("dalvik.vm.heaptargetutilization", "0.75");
+        property_set("dalvik.vm.heapminfree", "2m");
+        property_set("dalvik.vm.heapmaxfree", "8m");
     }
 
-    android::init:property_set("ro.telephony.default_network", "10");
+    property_set("ro.telephony.default_network", "10");
 
     sku = GetProperty("ro.boot.hardware.sku","");
     if (sku == "XT1600") {
@@ -112,18 +111,18 @@ void vendor_load_properties()
     } else if (sku == "XT1601") {
         /* XT1601 */
         customerid = "retail";
-        android::init::property_set("persist.radio.process_sups_ind", "1");
+        property_set("persist.radio.process_sups_ind", "1");
         if (msim) {
-            android::init::property_set("persist.radio.pb.max.match", "8");
-            android::init::property_set("persist.radio.pb.min.match", "8");
+            property_set("persist.radio.pb.max.match", "8");
+            property_set("persist.radio.pb.min.match", "8");
         }
     } else if (sku == "XT1602") {
         /* XT1602 */
     } else if (sku == "XT1603") {
         /* XT1603 */
         customerid = "retail";
-        android::init::property_set("persist.radio.pb.max.match", "10");
-        android::init::property_set("persist.radio.pb.min.match", "7");
+        property_set("persist.radio.pb.max.match", "10");
+        property_set("persist.radio.pb.min.match", "7");
     } else if (sku == "XT1604") {
         /* XT1604 - HAS NFC! */
     } else if (sku == "XT1607") {
@@ -133,7 +132,7 @@ void vendor_load_properties()
     }
 
     if (customerid) {
-        android::init::property_set("ro.mot.build.customerid", customerid);
+        property_set("ro.mot.build.customerid", customerid);
     }
 
 }
